d7146b6db8af59a53e1e77de73ded96d5cfdb842
Nguyen Chi Tam <xomrayno5@gmail.com>
2021-10-04 17:30:44 +0700

    update 4/10/2021, add oop, hackerrank code in solution.py file,handleexception

---
+++ handleException.py
@@ -0,0 +1,42 @@
+# import module sys to get the type of exception
+import sys
+
+randomList = ['a', 0, 2]
+
+for entry in randomList:
+    try:
+        print("The entry is", entry)
+        r = 1/int(entry)
+        break
+    except Exception as e:
+        print("Oops!", e.__class__, "occurred.")
+        print("Next entry.")
+        print()
+    finally:
+        print("Good")
+print("The reciprocal of", entry, "is", r)
+
+
+
+
+
+class SalaryNotInRangeError(Exception):
+    """Exception raised for errors in the input salary.
+
+    Attributes:
+        salary -- input salary which caused the error
+        message -- explanation of the error
+    """
+
+    def __init__(self, salary, message="Salary is not in (5000, 15000) range"):
+        self.salary = salary
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self): #override từ Exception
+        return f'{self.salary} -> {self.message}'
+
+
+salary = int(input("Enter salary amount: "))
+if not 5000 < salary < 15000:
+    raise SalaryNotInRangeError(salary)
